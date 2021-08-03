37dcb30be1cbca7bd6ec956cdeeecb92614091c7
yyc <417004794@qq.com>
2021-08-03 14:11:47 +0800

    db.properties支持指定minimum-idle、maximum-pool-size参数

--- src/main/java/com/joker17/sql/small/tools/utils/JdbcUtils.java
+++ src/main/java/com/joker17/sql/small/tools/utils/JdbcUtils.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.io.*;
+import java.util.Optional;
 import java.util.Properties;
 
 public class JdbcUtils {
@@ -25,8 +26,13 @@ public class JdbcUtils {
         hikariDataSource.setPassword(properties.getProperty("datasource.password"));
         hikariDataSource.setJdbcUrl(properties.getProperty("datasource.url"));
         hikariDataSource.setDriverClassName(properties.getProperty("datasource.driver-class-name"));
-        hikariDataSource.setMinimumIdle(1);
-        hikariDataSource.setMaximumPoolSize(20);
+
+        hikariDataSource.setMinimumIdle(Optional.ofNullable(properties.getProperty("datasource.minimum-idle"))
+                .map(Integer::valueOf).orElse(1));
+        hikariDataSource.setMaximumPoolSize(Optional.ofNullable(properties.getProperty("datasource.maximum-pool-size"))
+                .map(Integer::valueOf).orElse(20));
+        Optional.ofNullable(properties.getProperty("datasource.connection-timeout"))
+                .ifPresent((it) -> hikariDataSource.setConnectionTimeout(Long.valueOf(it)));
         return hikariDataSource;
     }
 
